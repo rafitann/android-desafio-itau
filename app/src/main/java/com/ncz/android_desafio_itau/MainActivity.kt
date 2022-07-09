279637f35c78c3c6f814abe71299051f25dc315a
rafaela.narcizo <narcizorafaela@gmail.com>
2022-07-09 20:20:41 -0300

    implementing libs: navigation

---
+++ app/src/main/java/com/ncz/android_desafio_itau/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ncz.android_desafio_itau
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

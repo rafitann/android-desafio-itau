1bf0d53fea2f569be07d01b020a01a20feefbd5a
rafaela.narcizo <narcizorafaela@gmail.com>
2022-07-09 20:35:03 -0300

    creating the screens

---
+++ app/src/main/java/com/ncz/android_desafio_itau/DetailsFragment.kt
@@ -0,0 +1,6 @@
+package com.ncz.android_desafio_itau
+
+import androidx.fragment.app.Fragment
+
+class DetailsFragment : Fragment(R.layout.fragment_details) {
+}
---
+++ app/src/main/java/com/ncz/android_desafio_itau/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.ncz.android_desafio_itau
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+}

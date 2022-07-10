d728c05a6ea11af50e06b42bb3925484ba5f4859
rafaela.narcizo <narcizorafaela@gmail.com>
2022-07-09 22:08:44 -0300

    adjustments in CategoryDataSource

--- app/src/main/java/com/ncz/android_desafio_itau/data/datasource/CategoryDataSource.kt
+++ app/src/main/java/com/ncz/android_desafio_itau/data/datasource/CategoryDataSource.kt
@@ -2,10 +2,9 @@ package com.ncz.android_desafio_itau.data.datasource
 
 import com.ncz.android_desafio_itau.data.network.api.RetrofitInstance
 import com.ncz.android_desafio_itau.data.network.service.CategoryService
-import com.ncz.android_desafio_itau.data.network.service.ReleasesService
 import com.ncz.android_desafio_itau.domain.model.Category
 
-class CategoryDataSource {
+open class CategoryDataSource {
     private val categoryService by lazy { RetrofitInstance.retrofit.create(CategoryService::class.java)}
 
     suspend fun getCategory(): List<Category> {

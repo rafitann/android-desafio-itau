db5536608d2f0333d9f41fbbdd2132b8de6fe64c
rafaela.narcizo <narcizorafaela@gmail.com>
2022-07-10 16:24:53 -0300

    add the Splash View

---
+++ app/src/main/java/com/ncz/android_desafio_itau/app/view/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.ncz.android_desafio_itau.app.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.ncz.android_desafio_itau.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+    private val handler by lazy {
+        Handler(mainLooper)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        handler.postDelayed({
+            startActivity(Intent(this, BaseActivity::class.java))
+            finish()
+        }, 1500)
+    }
+
+}

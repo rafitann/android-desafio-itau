2ce309753d7d4737e8fb97839d37ba6bcdc8f81d
rafaela.narcizo <narcizorafaela@gmail.com>
2022-07-10 02:08:39 -0300

    adapter creation

--- app/src/main/java/com/ncz/android_desafio_itau/app/view/HomeFragment.kt
+++ app/src/main/java/com/ncz/android_desafio_itau/app/view/HomeFragment.kt
@@ -1,8 +1,23 @@
 package com.ncz.android_desafio_itau.app.view
 
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.ncz.android_desafio_itau.R
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
+    private fun teste(){
+        val image = ImageView(requireContext())
+        image.setImageResource(R.drawable.ic_health_and_beauty)
+    }
+
+
+    companion object Categories{
+        const val TRANSPORT = "Transporte"
+        const val SHOPPING_ONLINE = "Compras Online"
+        const val HEALTH_AND_BEAUTY = "Saúde e Beleza"
+        const val AUTOMOTIVE_SERVICES = "Serviços Automotivos"
+        const val RESTAURANTS= "Restaurantes"
+        const val SUPERMARKET = "Super Mercados"
+    }
 }
--- app/src/main/java/com/ncz/android_desafio_itau/app/view/adapters/HomeAdapter.kt
+++ app/src/main/java/com/ncz/android_desafio_itau/app/view/adapters/HomeAdapter.kt
@@ -1,4 +1,30 @@
 package com.ncz.android_desafio_itau.app.view.adapters
 
-class HomeAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ncz.android_desafio_itau.databinding.ReleaseCardBinding
+import com.ncz.android_desafio_itau.domain.model.Release
+
+class HomeAdapter(private val releases: MutableList<Release>) :
+    RecyclerView.Adapter<HomeAdapter.ReleaseViewHolder>() {
+
+    inner class ReleaseViewHolder(binding: ReleaseCardBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val originName = binding.originName
+        val value = binding.value
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReleaseViewHolder {
+        val binding = ReleaseCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ReleaseViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: HomeAdapter.ReleaseViewHolder, position: Int) {
+        holder.originName.text = releases[position].origem
+        holder.value.text = releases[position].valor.toString()
+        releases[position].categoria
+    }
+
+    override fun getItemCount() = releases.size
 }
